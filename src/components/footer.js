a71330ec2832dd86ae1ccbf600f6d678aff7adbf
Ollie-Judge <ollie.judge@hotmail.com>
2022-07-19 17:23:52 +0100

    styled the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,46 @@
 import React from "react";
 import "../styling/footer.css";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-box">
-        <div>
-          <h6>Room Service</h6>
-        </div>
+      <div>
+        <h6>Room Service</h6>
+      </div>
 
-        <div className="socialMedia"></div>
+      <div className="socialMedia">
+        <ul>
+          <li>
+            <a
+              href="https://en-gb.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook className="icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="icon" />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
